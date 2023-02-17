a6065a9ac2f50b41fd13a750f178e9aab2f344ee
YashKumar0416 <122213030+YashKumar0416@users.noreply.github.com>
2023-02-17 06:20:21 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKQrNVjxQnXnchm4ecz8_EpoGDOO_RCXQ",
-  authDomain: "fir-react-bef6f.firebaseapp.com",
-  projectId: "fir-react-bef6f",
-  storageBucket: "fir-react-bef6f.appspot.com",
-  messagingSenderId: "400908449085",
-  appId: "1:400908449085:web:d51cd2adf509d0e0318154",
-  measurementId: "G-9NVZ96JRWE"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
